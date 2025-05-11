50869366201d318856c1789ce5635893cfc10d56
kishoretk12 <kishore.kumar0728@gmail.com>
2025-05-11 18:45:41 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+      <h1 className="text-4xl font-bold text-blue-600">Kishore T K</h1>
+      <p className="text-xl mt-2">Cloud / DevOps Engineer</p>
+      <div className="mt-4 space-x-4">
+        <a href="https://www.linkedin.com/in/kishore-tk" className="text-blue-500 hover:underline">LinkedIn</a>
+        <a href="https://github.com/kishoretk12" className="text-blue-500 hover:underline">GitHub</a>
+        <a href="mailto:kishore.kumar0728@gmail.com" className="text-blue-500 hover:underline">Email</a>
+      </div>
+    </div>
+  );
+};
+
+export default App;
